b56835895799baa19bf02a22ff162eb13eccc1fc
Fleursail <h098s02610@chunchon.gwe.hs.kr>
2025-10-31 15:24:16 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,14 +5,14 @@ import pandas as pd
 
 # --- 1. 페이지 기본 설정 ---
 st.set_page_config(
-    page_title="Streamlit 마법 교실",
+    page_title="발디의 수학 교실",
     page_icon="🔮",
     layout="wide"
 )
 
 # --- 2. 페이지 타이틀 ---
-st.title("Streamlit 마법 교실 🔮")
-st.subheader("HTML/CSS를 활용해 멋진 효과를 만들어 봐요!")
+st.title("발디의 수학 교실 🔮")
+st.subheader("수학 문재를 풀어보고 선생님한테서 도망쳐요!")
 st.markdown("---") # 구분선
 
 # --- 3. 모든 커스텀 CSS ---
